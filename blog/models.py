674f3357d99690d7b5aa1603e8edf83661fbba51
german <germantuppo@gmail.com>
2022-06-26 08:01:56 -0300

    creando models blog y contacto. agregue apps a settings.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField()
+    pais= models.CharField(max_length=20)
+    deseaInfo= models.BooleanField()
+    idioma= models.CharField(max_length=2)
+    fechaRegistro= models.DateTimeField()
+    avatar=models.CharField(max_length=50)
+
+class Comentario(models.Model):
+    usuarioId= models.IntegerField()
+    fechaHora= models.DateTimeField()
+    contenido= models.CharField(max_length=500)
--- contacto/models.py
+++ contacto/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Consulta(models.Model):
+    fechaHora= models.DateTimeField()
+    email= models.EmailField()
+    contenido= models.CharField(max_length=500)
+
+class ArmaViaje(models.Model):
+    fechaHora= models.DateTimeField()
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField()
+    pais= models.CharField(max_length=20)
+    idioma= models.CharField(max_length=10)
+    telefono= models.CharField(max_length=15)
+    fechaLlegada= models.DateField()
+    fechasFlexibles= models.BooleanField()
+    destinos=models.JSONField()
+    cantidadNoches= models.IntegerField()
+    adultos= models.IntegerField()
+    menores= models.IntegerField()
+    presupuesto= models.IntegerField()
+    nivelAlojamiento= models.CharField(max_length=10)
+    vuelosInternacionales= models.BooleanField()
+    comentario= models.CharField(max_length=500)
