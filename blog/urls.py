dd995564e1a2c8d0f44eaaeb818dcce6078e9c27
german <germantuppo@gmail.com>
2022-06-29 04:16:19 -0300

    arreglando usuario form que no lo había agregado tampoco

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from blog import views
 urlpatterns = [
    
     path('', views.blog, name="blog"), #esta sería la vista blog
-
+    path('usuarioFormulario/', views.usuarioFormulario, name="usuarioFormulario"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,46 @@ from typing import List
 from django.http.request import QueryDict
 from django.shortcuts import redirect, render, HttpResponse
 from django.http import HttpResponse
-#from AppCoder.models import Curso, Profesor #Import models
-#from AppCoder.forms import CursoFormulario, ProfesorFormulario #Import Forms
+from blog.models import Usuario, Comentario #Import models
+from blog.forms import UsuarioFormulario, ComentarioFormulario #Import Forms
 
+#esto aun no lo estoy usando
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+
+import datetime
 # Create your views here.
 
 def blog(request):
-
       return render(request, "blog/blog.html")
+
+
+
+def usuarioFormulario(request):
+
+      if request.method == 'POST':
+
+            miFormulario = UsuarioFormulario(request.POST) #aquí mellega toda la información del html
+
+            print(miFormulario)
+
+            if miFormulario.is_valid:   #Si pasó la validación de Django
+
+                  informacion = miFormulario.cleaned_data
+                  print(informacion)
+                  usuario = Usuario (nombre=informacion['nombre'], apellido=informacion['apellido'], email=informacion['email'], pais=informacion['pais'], deseaInfo=informacion['deseaInfo'], idioma=informacion['idioma'], fechaRegistro=datetime.datetime.now(), avatar=informacion['avatar']) 
+
+                  #a ver si mete algo por default , fechaRegistro=informacion['fechaRegistro'], avatar=informacion['avatar']
+                  #aca tengo problemas con la fecha, por hacerme el canchero me pasa
+                  usuario.save()
+
+                  return render(request, "blog/blog.html") #Vuelvo al inicio o a donde quieran
+
+      else: 
+
+            miFormulario= UsuarioFormulario() #Formulario vacio para construir el html
+
+      return render(request, "blog/usuarioFormulario.html", {"miFormulario":miFormulario})
